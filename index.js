269c0f16afa7180e0d717a4b2671f139b618c352
Timothy Millward <silhouettetechnician@gmail.com>
2019-07-24 22:36:37 +0100

    bug fixing

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 require('dotenv').config()
-const router = require('express').Router()
+
 const {PORT, ENV} = require('./config/environment')
 const express = require('express')
-const request = require('request')
-const DarkSky = require('dark-sky')
-const bodyParser = require('body-parser')
+const request = require('request') 
 const cors = require('cors')
-const token = process.env.DARK_SKY_TOKEN
+// const token = process.env.DARK_SKY_TOKEN
 const app = express()
 
-app.use(bodyParser.json())
-app.use(express.json)
-app.use(cors());
 
+app.use(cors());
 app.use(express.static(`${__dirname}/dist`))
 
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
-
-app.get('/api/forecast', function(req, res) {
-    axios(`https://api.darksky.net/forecast/6fcc0f895857627606cae8c37cd1e787/37.8267,-122.4233`, function (error, response, body) {
-        console.log('error:', error); 
-        console.log('statusCode:', response && response.statusCode); 
+​
+app.post('/api/forecast', function (req, res) {
+​
+    request(`https://api.darksky.net/forecast/6fcc0f895857627606cae8c37cd1e787/37.8267,-122.4233`, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
         console.log('body:', body);
-        res.json(body);
+        res.send(body);
     })
 })
-
+​
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+​
+​
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
-
+​
 module.exports = app
