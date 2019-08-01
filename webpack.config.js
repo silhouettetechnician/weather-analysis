3e828ed34bb08bd3b16c36f59fbc3e394c57a18c
Timothy Millward <silhouettetechnician@gmail.com>
2019-08-01 17:25:02 +0100

    bug fixing

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: production,
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
